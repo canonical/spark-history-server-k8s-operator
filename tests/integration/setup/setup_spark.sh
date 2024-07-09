5d49faa01addfad4659175b367e62ad3391e51b5
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-07-09 15:40:38 +0200

    switch flag

--- tests/integration/setup/setup_spark.sh
+++ tests/integration/setup/setup_spark.sh
@@ -12,7 +12,7 @@ spark-client.service-account-registry create --username hello \
                 --conf spark.hadoop.fs.s3a.secret.key=$3 \
                 --conf spark.hadoop.fs.s3a.endpoint=$1 \
                 --conf spark.hadoop.fs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider \
-                --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false \
+                --conf spark.hadoop.fs.s3a.connection.ssl.enabled=true \
                 --conf spark.hadoop.fs.s3a.path.style.access=true \
                 --conf spark.eventLog.enabled=true \
                 --conf spark.eventLog.dir=s3a://history-server/spark-events/ \
