ddb034389dcee4ebb9a95feb128eb27a2d25e495
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-09-11 12:43:04 +0000

    Fix retrival of access and secret key

--- tests/integration/setup/setup_minio.sh
+++ tests/integration/setup/setup_minio.sh
@@ -6,11 +6,19 @@ do
   echo "s3 params setup attempt=$attempt"
   if [ -z "$access_key" ]; then
     access_key=$(sudo microk8s.kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_ACCESS_KEY}' | base64 -d)
-    echo "access_key=$access_key"
+    if [ $? -eq 0 ]; then
+      echo "access_key=$access_key"
+    else
+      echo "access_key=minio"
+    fi
   fi
   if [ -z "$secret_key" ]; then
     secret_key=$(sudo microk8s.kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_SECRET_KEY}' | base64 -d)
-    echo "secret_key=$secret_key"
+    if [ $? -eq 0 ]; then
+      echo "secret_key=$secret_key"
+    else
+      echo "secret_key=minio123"
+    fi
   fi
   if [ -z "$endpoint_ip" ]; then
     endpoint_ip=$(sudo microk8s.kubectl get services -n minio-operator | grep minio | awk '{ print $3 }')
