ebb372c9622af24a5a3677c612b0b0e329fc0869
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-11-11 20:40:06 +0000

    Add logs

--- tests/integration/setup/run_spark_job.sh
+++ tests/integration/setup/run_spark_job.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 spark-client.spark-submit --username hello --conf spark.kubernetes.executor.request.cores=0.1 --class org.apache.spark.examples.SparkPi local:///opt/spark/examples/jars/spark-examples_2.12-3.4.2.jar 100
+echo "Print logs"
+kubectl logs -l app.kubernetes.io/managed-by:spark8t
