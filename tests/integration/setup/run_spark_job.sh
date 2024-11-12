98c7bf6d9bf7a0159a810ad67269b778ced31f72
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-11-12 08:41:24 +0000

    fix logging

--- tests/integration/setup/run_spark_job.sh
+++ tests/integration/setup/run_spark_job.sh
@@ -2,4 +2,4 @@
 
 spark-client.spark-submit --username hello --conf spark.kubernetes.executor.request.cores=0.1 --class org.apache.spark.examples.SparkPi local:///opt/spark/examples/jars/spark-examples_2.12-3.4.2.jar 100
 echo "Print logs"
-kubectl logs -l app.kubernetes.io/managed-by:spark8t
+kubectl logs -l spark-version=3.4.2
