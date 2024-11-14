14fb0599f4ddb26941ebe1a14b680a708f3bae67
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-11-14 20:33:38 +0000

    Increase timeout and logs

--- tests/integration/setup/run_spark_job.sh
+++ tests/integration/setup/run_spark_job.sh
@@ -3,3 +3,5 @@
 spark-client.spark-submit --username hello --conf spark.kubernetes.executor.request.cores=0.1 --class org.apache.spark.examples.SparkPi local:///opt/spark/examples/jars/spark-examples_2.12-3.4.2.jar 1000
 echo "Print logs"
 kubectl logs -l spark-version=3.4.2
+echo "Kubectl get pods -A"
+kubectl get pods -A
