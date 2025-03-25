f6abcd0235077c627cd0c9180c197ef5e859268e
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-03-25 19:32:40 +0100

    fix

--- tests/integration/setup/get_image_metadata.sh
+++ tests/integration/setup/get_image_metadata.sh
@@ -3,7 +3,7 @@
 IMAGE=$1
 
 docker pull $IMAGE >/dev/null 2>/dev/null
-METADATA=$(docker image inspect ghcr.io/canonical/charmed-spark:3.4-22.04_edge 2>/dev/null | jq '.[0].Config.Labels')
+METADATA=$(docker image inspect $IMAGE 2>/dev/null | jq '.[0].Config.Labels')
 # docker rmi $IMAGE
 
 echo $METADATA
