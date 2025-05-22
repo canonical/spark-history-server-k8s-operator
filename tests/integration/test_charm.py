81653fd8b98c6a1d59d19137afb9a11b4bfb1777
Paolo Sottovia <paolo.sottovia@canonical.com>
2025-05-22 14:00:36 +0000

    more debug

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -121,7 +121,8 @@ def test_build_and_deploy(
             apps = json.loads(
                 urllib.request.urlopen(f"http://{address}:18080/api/v1/applications").read()
             )
-        except Exception:
+        except Exception as e:
+            logger.error(e)
             sleep(3)
 
     assert apps is not None and len(apps) == 0
