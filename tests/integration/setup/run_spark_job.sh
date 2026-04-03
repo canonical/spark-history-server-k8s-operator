8578f2c0d9ce398fe184878311988dd48baa0383
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-04-03 09:05:37 +0545

    Bump scala version to 2.13 in tests

--- tests/integration/setup/run_spark_job.sh
+++ tests/integration/setup/run_spark_job.sh
@@ -2,7 +2,7 @@
 
 SPARK_VERSION=$1
 
-spark-client.spark-submit --username hello --conf spark.kubernetes.executor.request.cores=0.1 --class org.apache.spark.examples.SparkPi local:///opt/spark/examples/jars/spark-examples_2.12-${SPARK_VERSION}.jar 1000
+spark-client.spark-submit --username hello --conf spark.kubernetes.executor.request.cores=0.1 --class org.apache.spark.examples.SparkPi local:///opt/spark/examples/jars/spark-examples_2.13-${SPARK_VERSION}.jar 1000
 echo "Print logs"
 kubectl logs -l spark-version=${SPARK_VERSION}
 echo "Kubectl get pods -A"
