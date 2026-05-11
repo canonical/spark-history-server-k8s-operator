0ac2db11bb15ca691264fcd368a085db730e0538
Alex Batisse <alex.batisse@canonical.com>
2026-05-11 11:33:50 +0200

    chore: Migrate image inspection to skopeo

--- tests/integration/setup/get_image_metadata.sh
+++ tests/integration/setup/get_image_metadata.sh
@@ -2,8 +2,6 @@
 
 IMAGE=$1
 
-docker pull $IMAGE >/dev/null 2>/dev/null
-METADATA=$(docker image inspect $IMAGE 2>/dev/null | jq '.[0].Config.Labels')
-# docker rmi $IMAGE
+METADATA=$(rockcraft.skopeo inspect "docker://${IMAGE}" 2>/dev/null | jq '.Labels')
 
 echo $METADATA
