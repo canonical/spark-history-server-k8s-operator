f6bbaa46a0b33114eef860476647fb67904c8aa4
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-06-04 10:02:26 +0200

    [PRA-287] Use S3 Integrator from track 2 and use object storage libs from object-storage-charmlib (#173)

--- tests/integration/test_charm_tls.py
+++ tests/integration/test_charm_tls.py
@@ -72,7 +72,7 @@ def test_build_and_deploy(
     juju.wait(jubilant.all_agents_idle, timeout=1000)
 
     logger.info("Setting up s3 credentials in s3-integrator charm")
-    set_s3_credentials(juju, access_key, secret_key)
+    set_s3_credentials(juju, charm_versions.s3.application_name, access_key, secret_key)
 
     ca = get_certificate_from_file(tls_ca_chain_path)
     ca_b64 = base64.b64encode(ca.encode("utf-8")).decode("utf-8")
